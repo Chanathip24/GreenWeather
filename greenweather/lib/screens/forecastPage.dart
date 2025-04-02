2c88d06515f8c5a76c2f9736e76447166f154ce8
PhawitTop <phawitphan@gmail.com>
2025-04-02 21:12:30 +0700

    test2

--- greenweather/lib/screens/forecastPage.dart
+++ greenweather/lib/screens/forecastPage.dart
@@ -1,23 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:greenweather/widgets/Appbar.dart';
+import '../widgets/Navbar.dart';
 
 class ForecastPage extends StatefulWidget {
-  const ForecastPage({super.key});
+  const ForecastPage({Key? key}) : super(key: key);
 
   @override
-  State<ForecastPage> createState() => ForecastPageState();
+  State<ForecastPage> createState() => _ForecastPageState(); // Fix: Use correct class name
 }
 
-class ForecastPageState extends State<ForecastPage> {
-  @override
+class _ForecastPageState extends State<ForecastPage> {
+  final List<Map<String, String>> weatherData = [
+    {'date': '8 มีนาคม', 'temp': '32°C', 'condition': 'มีแดดจัด'},   
+    {'date': '9 มีนาคม', 'temp': '30°C', 'condition': 'มีเมฆบางส่วน'},
+    {'date': '10 มีนาคม', 'temp': '31°C', 'condition': 'มีแดดจัด'},
+    {'date': '11 มีนาคม', 'temp': '29°C', 'condition': 'ฝนตก'},
+    {'date': '12 มีนาคม', 'temp': '28°C', 'condition': 'มีเมฆมาก'},
+    {'date': '13 มีนาคม', 'temp': '30°C', 'condition': 'มีแดดจัด'},]; 
+    
+    @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       appBar: AppBar(
-        title: Text("Weather Forecast"),
+        title: Text("Weather forecast"),
       ),
-      body: SafeArea(
-          child: Column(
-        children: [Text("Forecastpage")],
-      )),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+        child: Column(
+          children: weatherData.asMap().entries.map((entry) {
+            int index = entry.key;
+            Map<String, String> data = entry.value;
+            return WeatherCard(
+              date: data['date']!,
+              temp: data['temp']!,
+              condition: data['condition']!,
+              isFirst: index == 0,
+            );
+          }).toList(),
+        ),
+      ),
+      ),
     );
   }
 }
+class WeatherCard extends StatelessWidget {
+  final String date;
+  final String temp;
+  final String condition;
+  final bool isFirst;
+
+  WeatherCard({required this.date, required this.temp, required this.condition,required this.isFirst});
+    @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: isFirst ? Colors.green.shade100 : const Color.fromARGB(255, 221, 241, 201),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        padding: EdgeInsets.all(16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'วันนี้',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                Text(
+                  date,
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+            CircleAvatar(
+              backgroundColor: Colors.yellow,
+              radius: 24,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  temp,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                ),
+                Text(
+                  condition,
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
--- greenweather/lib/screens/leaderboardPage.dart
+++ greenweather/lib/screens/leaderboardPage.dart
@@ -1,20 +1,130 @@
 import 'package:flutter/material.dart';
 
-class Leaderboardpage extends StatefulWidget {
-  const Leaderboardpage({super.key});
+class Leaderboardpage extends StatelessWidget {
+  Leaderboardpage({Key? key}) : super(key: key);
 
-  @override
-  State<Leaderboardpage> createState() => _LeaderboardpageState();
-}
+  final List<Map<String, dynamic>> rankings = [
+    {"username": "@ecoguardian", "score": 5220},
+    {"username": "@ecoguardian", "score": 4220},
+    {"username": "@ecoguardian", "score": 4219},
+  ];
 
-class _LeaderboardpageState extends State<Leaderboardpage> {
+  final int userRank = 5;
+  final int userScore = 820;
+  final int pointsToNextRank = 3000;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: Column(
-        children: [Text("Leaderboard")],
-      )),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.green[100],
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "แชมป์ประจำสัปดาห์",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  const Text("รับ 5-10 แต้ม ต่อการรีวิว"),
+                  const SizedBox(height: 10),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      child: const Text("ดูแชมป์ประจำสัปดาห์",
+                      style: TextStyle(
+                          color: Colors.white,),
+                      )
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: rankings.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    color: Colors.green[50],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: ListTile(
+                      title: Text(
+                        "${index + 1}. ${rankings[index]['username']}",
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: const Text("20 รีวิว  14 รูปภาพ  7 วันติดต่อกัน"),
+                      trailing: Text(
+                        "${rankings[index]['score']} แต้ม",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.grey[200],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "คุณอยู่อันดับที่ $userRank",
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    "$userScore แต้ม",
+                    style: const TextStyle(fontSize: 18, color: Colors.green, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 10),
+                  Text("อีก $pointsToNextRank แต้มเพื่อขึ้นอันดับที่ 4"),
+                  const SizedBox(height: 10),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      child: const Text("ส่งรีวิวเพิ่มเพื่อรับแต้ม",
+                      style: TextStyle(
+                          color: Colors.white,),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
